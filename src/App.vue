f4cb77d59f06f52352068d2255b3cbe9fbcfded7
gustavojim <113261314+gustavojim@users.noreply.github.com>
2022-10-20 14:52:15 +0200

    Creacion de router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <h1>Task App</h1>
+  <ul>
+        <li>
+            <router-link :to="{name: 'home'}">Home</router-link>
+        </li>
+        <li>
+            <router-link :to="{name: 'login'}">Login</router-link>
+        </li>
+    </ul>
+    <router-view></router-view>
 </template>
 
 <script setup>
-
+console.log(process.env.NODE_ENV)
 </script>
 
 <style scoped>
