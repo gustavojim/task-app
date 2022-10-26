9af846c788c33f9b33445690c6eff12a7bcbe0f8
gustavojim <113261314+gustavojim@users.noreply.github.com>
2022-10-27 00:41:32 +0200

    Pages nuevo intento

---
+++ docs/assets/file_Auth.daa52175.js
@@ -0,0 +1,1 @@
+import{_ as o,A as t,o as n,c as r,l as c,F as s,a}from"./file_index.9630356a.js";const _={},l=a("h1",null,null,-1);function u(m,d){const e=t("router-view");return n(),r(s,null,[l,c(e)],64)}const i=o(_,[["render",u]]);export{i as default};
--- docs/assets/file_EmailVerification.c3edd32f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./file_index.3bb71f9d.js";const o={};function r(t,a){return c(),n("h1",null,"Email Verification")}const s=e(o,[["render",r]]);export{s as default};
